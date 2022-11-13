ee557a6a79db92a439878637500d6ed6a0c5ffd6
Amir Hamidi <Amirarsalanh2001@gmail.com>
2022-11-13 09:42:24 +0330

    Added media files

--- dentacare/settings.py
+++ dentacare/settings.py
@@ -116,6 +116,11 @@ STATIC_URL = "static/"
 STATIC_ROOT = "static/"
 STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
 
+# Media files (Images)
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = "media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
